08117d5846b70964b455744dd8e9f6e101a5912c
Julia <142966227+juliaA12323@users.noreply.github.com>
2023-10-11 15:32:13 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,97 @@
+/*THINGS TO DO
+1. finalise questions
+2. add code to add picture for each choice
+3. fix the add of design for each choice
+4. direct to spoonacular page
+5. use vue(?)
+*/
+
+let questions = [
+    // { 
+    //     question: 'Serving size', 
+    //     choices: ['1','2','3','4+']
+    // },
+    { 
+        question: 'Dietary requirements', 
+        choices: ['Vegetarian', 'Vegan', 'Gluten-free', 'Halal', 'Keto', 'None']
+    },
+    { 
+        question: 'Which course', 
+        choices: ['Appetizer', 'Mains', 'Dessert']
+    },
+    // { 
+    //     question: 'Main Flavour', 
+    //     choices: ['Sweet', 'Sour', 'Salty','Bitter','Umami','Spicy']
+    // },
+    { 
+        question: 'Prep time', 
+        choices: ['<15mins', '15-25mins','25-35min','45+ mins']
+    },
+    { 
+        question: 'Cuisine', 
+        choices: ['Japanese', 'Chinese', 'Italian', 'Indian', 'Mexican']
+    },
+    { 
+        question: 'Choice of Protein', 
+        choices: ['Chicken', 'Pork', 'Beef', 'Fish','Tofu']
+    }
+];
+
+let questionKeys = {
+    'Dietary requirements': 'diet',
+    'Which course': 'meal type',
+    'Prep time': 'maxReadyTime',
+    'Cuisine': 'cuisine',
+    'Choice of Protein': ['query', 'includeIngredients']
+};
+
+let currentQuestionIndex = 0;
+let answers = {};
+
+function displayCurrentQuestion() {
+    var questionContainer = document.getElementById('question-container');
+    var optionsContainer = document.getElementById('options-container');
+    
+    questionContainer.innerHTML = '';
+    optionsContainer.innerHTML = '';
+    
+    // show question
+    var currentQuestion = questions[currentQuestionIndex];
+    var questionElement = document.createElement('h2');
+    questionElement.innerText = currentQuestion.question;
+    questionContainer.appendChild(questionElement);
+
+    // show choices
+    var choices = currentQuestion.choices;
+    for (let index = 0; index < choices.length; index++) {
+        var choice = choices[index];
+        var choiceDiv = document.createElement('div');
+        choiceDiv.className = 'choices'; //put color
+        var choiceButton = document.createElement('button');
+        choiceButton.innerText = choice;
+        choiceButton.addEventListener('click', () => displayChoices(choice, currentQuestion));
+        optionsContainer.appendChild(choiceButton);
+    };
+
+    currentQuestionIndex++;
+
+    //display everything
+}
+
+function displayChoices(choice, currentQuestion){
+    var key = questionKeys[currentQuestion.question];
+            if (Array.isArray(key)) {
+                for (let i = 0; i < key.length; i++) {
+                    answers[key[i]] = choice;
+                }
+            } else {
+                answers[key] = choice;
+            }
+    if (currentQuestionIndex < questions.length) {
+        displayCurrentQuestion();
+    } else {
+        // All questions answered; note: redirect to spoonacular
+        document.write('done')
+        console.log(answers)
+    }
+}
