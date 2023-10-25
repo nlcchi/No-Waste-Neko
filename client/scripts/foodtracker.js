e58c46da57c528881015471e6c4c6f9f0dd1a459
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2023-10-25 18:35:39 +0800

    change colour and sorted

--- client/scripts/foodtracker.js
+++ client/scripts/foodtracker.js
@@ -1,27 +1,43 @@
 // to be changed after creating API and db
-var ingredients = [{"Name": "chicken", "Category": "Meat", "Exp": "2023-10-23"}, {"Name": "carrot", "Category": "Vegetable", "Exp": "2023-10-26"}, {"Name": "tomato", "Category": "Vegetable", "Exp": "2023-10-15"}, {"Name": "cabbage", "Category": "Vegetable", "Exp": "2023-10-10"}];
+var ingredients = [{"Name": "chicken", "Category": "Meat", "Exp": "2023-10-23"}, {"Name": "carrot", "Category": "Vegetable", "Exp": "2023-10-26"}, {"Name": "tomato", "Category": "Vegetable", "Exp": "2023-10-15"}, {"Name": "cabbage", "Category": "Vegetable", "Exp": "2023-11-30"}];
 
-var displayIngre = document.getElementById("front-row");
-for (ingredient of ingredients) {
-    // console.log(ingredient.Exp);
-    var exp = (ingredient.Exp).split('-');
+// Helper function to calculate days till expiry
+function getDaysTillExpiry(expiryDate) {
+    var exp = expiryDate.split('-');
     var year = exp[0];
     var month = exp[1] - 1;
     var day = exp[2];
     let ExpDate = new Date(year, month, day);
-    // console.log(ExpDate, typeof(ExpDate));
     var tdy = new Date();
-    // console.log(ExpDate.getTime(), tdy.getTime());
     var diffInDays = Math.ceil((ExpDate.getTime() - tdy.getTime()) / (1000 * 3600 * 24));
-    // console.log(diffInDays)
+    return diffInDays;
+}
+
+// Custom sort function
+ingredients.sort((a, b) => {
+    let daysA = getDaysTillExpiry(a.Exp);
+    let daysB = getDaysTillExpiry(b.Exp);
+
+    if (daysA <= 0 && daysB > 0) return -1;
+    if (daysB <= 0 && daysA > 0) return 1;
+    if (daysA > 0 && daysA <= 7 && daysB > 7) return -1;
+    if (daysB > 0 && daysB <= 7 && daysA > 7) return 1;
+
+    return daysA - daysB;
+});
+
+
+var displayIngre = document.getElementById("front-row");
+for (ingredient of ingredients) {
+    var diffInDays = getDaysTillExpiry(ingredient.Exp);
 
     if (diffInDays <= 0) {
         displayIngre.innerHTML += `
         <div class="col mb-3">
             <div class="card" >
-                <div class="card-body" >
-                    <h5 class="card-title text-danger">${ingredient.Name}</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">${ingredient.Category}</h6>
+                <div class="card-body bg-danger-subtle" >
+                    <h5 class="card-title"><span style="color: red;">${ingredient.Name}</span></h5>
+                    <h6 class="card-subtitle mb-2">${ingredient.Category}</h6>
                     <ul>
                         <li>Expiration Date: ${ingredient.Exp}</li>
                         <li>Days Till Expiry: <span style="color: red;">Expired!</span></li>
@@ -40,7 +56,7 @@ for (ingredient of ingredients) {
         displayIngre.innerHTML += `
         <div class="col mb-3">
             <div class="card" >
-                <div class="card-body" >
+                <div class="card-body bg-warning-subtle" >
                     <h5 class="card-title">${ingredient.Name}</h5>
                     <h6 class="card-subtitle mb-2 text-body-secondary">${ingredient.Category}</h6>
                     <ul>
