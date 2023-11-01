6afe634c4ce0d4ca8b9ce3494eada48c67c1ed7b
chloekkk <127963044+chloekkk@users.noreply.github.com>
2023-11-01 13:20:13 +0800

    Update process_signup.php

--- backend/process_signup.php
+++ backend/process_signup.php
@@ -2,44 +2,40 @@
 require_once "common.php";
 
     $errors = [];
+    $response = array();
 
     // Get the data from form processing
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $c_password = $_POST['c_password'];
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+    $c_password = $_GET['c_password'];
     
     // Check if username is already taken
     $dao = new UserDAO();
 
     if($dao->get($username)) {
+        http_response_code(409);
         $errors[] = "Username is already taken.";
-        $_SESSION['errors'] = $errors;
-        header("Location: signuppage.php?&username={$username}");
-        exit();
-    }
-
-    if($password != $c_password) {
+    } elseif($password != $c_password) {
+        http_response_code(409);
         $errors[] = "Passwords do not match";
     }
-
-    $_SESSION['errors'] = $errors;
 
     // if everything is checked. Create user Object and write to database
     if($errors==[]) {
         $passwordHash = password_hash($password, PASSWORD_DEFAULT);
         $user = new User($username, $passwordHash);
         $status = $dao->create($user);
-        var_dump($passwordHash);
-    } else {
-        header("Location: ../client/view/signuppage.php?&username={$username}");
+
+        if ($status) {
+            http_response_code(200);
+
+            $response = [
+                "message" => "Signup successful"
+            ];
+        }
     }
 
-if ($status) {
-    // success; redirect page
-    $_SESSION["login_page"] = $username;
-    header("Location: ../client/view/loginpage.php?username={$username}");
-    exit();
-}
+    $response["errors"] = $errors;
+    echo json_encode($response);
     
 ?>
-
