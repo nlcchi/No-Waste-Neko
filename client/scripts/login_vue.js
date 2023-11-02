3934e4925e143660317a4f76ed943edf75dbd5d7
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2023-11-02 10:59:02 +0800

    changes in loading screen and home page

--- client/scripts/login_vue.js
+++ client/scripts/login_vue.js
@@ -11,6 +11,15 @@ const main = Vue.createApp({
 
     // Methods
     methods: {
+        funfact() {
+            let funfacts = ["Applesauce was the first food eaten in space.", "Pistachios aren’t nuts—they are actually fruits.", "The world’s most expensive spice is saffron.", "Broccoli contains more protein than steak!", "The most stolen food in the world is cheese.", "The most popular fruit in the world is the tomato.", "The most popular spice in the world is pepper.", "The most popular vegetable in the world is the potato.", "The most popular nut in the world is the almond.", "The most popular grain in the world is rice.", "The most popular meat in the world is pork.", "The most popular seafood in the world is shrimp.", "The most popular fish in the world is herring.", "The most popular beverage in the world is water.", "The most popular cheese in the world is mozzarella.", "The most popular herb in the world is basil.", "The most popular spice blend in the world is curry powder.", "The most popular condiment in the world is soy sauce.", "The most popular sauce in the world is salsa."];
+
+            let random = Math.floor(Math.random() * funfacts.length);
+            let funfact = funfacts[random];
+            console.log(funfact);
+            document.getElementById("funfact").innerText += funfact;
+        },
+
         showLoadingScreen() {
             document.getElementById('loading-screen').style.display = 'flex';
         },
@@ -48,6 +57,7 @@ const main = Vue.createApp({
 
     // Lifecycle Hook
     mounted() {
+        this.funfact();
         this.hideLoadingScreen();
     },
 })
