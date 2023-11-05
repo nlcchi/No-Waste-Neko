b3495d50b84bc501ffbf4da189585d803b811e93
chi nguyen <lccnguyen.2022A@scis.smu.edu.com>
2023-11-05 18:28:20 +0800

    signup_vue

--- backend/process_signup.php
+++ backend/process_signup.php
@@ -2,42 +2,53 @@
 require_once "common.php";
 header('Content-Type: application/json');
 
-    $errors = [];
-    $response = array();
+$errors = [];
+$response = array();
 
+// Get the data from form processing
+$jsonData = file_get_contents('php://input');
+$data = json_decode($jsonData); // Decode JSON string into PHP object
 
-    // Get the data from form processing
-    $data = file_get_contents('php://input');
-    $username = $data->username;
-    $password = $data->password;
-    
-    // Check if username is already taken
-    $dao = new UserDAO();
+// Check if the JSON is parsed correctly
+if ($data === null) {
+    http_response_code(400); // Bad Request
+    $errors[] = "Invalid JSON data.";
+    echo json_encode(["errors" => $errors]);
+    exit();
+}
 
-    if($dao->get($username)) {
-        http_response_code(409);
-        $errors[] = "Username is already taken.";
-    } elseif($password != $c_password) {
-        http_response_code(409);
-        $errors[] = "Passwords do not match";
+$username = $data->username ?? null;
+$password = $data->password ?? null;
+$c_password = $data->c_password ?? null;
+
+// Additional validation for username, password, and c_password could be performed here
+
+// Check if username is already taken
+$dao = new UserDAO();
+
+if ($dao->get($username)) {
+    http_response_code(409);
+    $errors[] = "Username is already taken.";
+} elseif ($password !== $c_password) {
+    http_response_code(409);
+    $errors[] = "Passwords do not match.";
+}
+
+// If there are no errors, proceed with user creation
+if (empty($errors)) {
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+    $user = new User($username, $passwordHash);
+    $status = $dao->create($user);
+
+    if ($status) {
+        http_response_code(200);
+        $response = [
+            "message" => "Signup successful"
+        ];
     }
-
-    // if everything is checked. Create user Object and write to database
-    if($errors==[]) {
-        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-        $user = new User($username, $passwordHash);
-        $status = $dao->create($user);
-
-        if ($status) {
-            http_response_code(200);
-
-            $response = [
-                "message" => "Signup successful"
-            ];
-        }
-    }
-
+} else {
     $response["errors"] = $errors;
-    echo json_encode($response);
-    
+}
+
+echo json_encode($response);
 ?>
---
+++ client/scripts/signup_vue.js
@@ -0,0 +1,46 @@
+const main = Vue.createApp({
+    // Data Properties
+    data() {
+        return {
+            form: {
+                username: '',
+                password: '',
+                c_password: '',
+            },
+        };
+    },
+
+    // Methods
+    methods: {
+        add_user() {
+            console.log("=== [START] add_user() ===")
+        
+        
+            let api_endpoint_url = "../../backend/process_signup.php"
+        
+            axios.post(api_endpoint_url, {
+                username: this.form.username,
+                password: this.form.password,
+                c_password: this.form.c_password,
+            })
+            .then((response) => {
+                console.log('Signup successful:', response.data);
+                window.location.href = "../view/loginpage.html";
+    
+            }).catch((error) => {
+                // ERROR
+                // Something went wrong
+                console.log(error.response ? error.response.data : error);
+            })
+        
+            console.log("=== [END] add_user() ===")
+        
+        }
+    },
+
+    // Lifecycle Hook
+    mounted() {
+    },
+})
+
+main.mount('#main');
